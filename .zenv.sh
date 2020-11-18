caf8b1e75f0772d39f9780627bc1a426eb27137f
dungphan90 <d.phan@ucl.ac.uk>
2020-11-18 05:46:27 -0600

    Fermilab SSH connections.

--- .zenv.sh
+++ .zenv.sh
@@ -1,4 +1,52 @@
 # Detached working tree
 alias dgit='git --git-dir ~/.dotfiles/.git --work-tree=$HOME'
 
+alias kinit='kinit ${USER}@FNAL.GOV'
+alias ll="ls -lGh"
+alias vim-zenv="vim ~/.zenv.sh"
+alias code-zenv="code ~/.zenv.sh"
+alias top="bpytop"
+alias vim="nvim"
 
+# FNAL connections
+function minosgpvm() {
+  if ! klist -s
+  then
+    kinit
+  fi
+
+  if [ $# -gt 0 ];
+  then
+    ssh -XY dphan@minosgpvm$1.fnal.gov
+  else
+    ssh -XY dphan@minosgpvm06.fnal.gov
+  fi
+}
+
+function novagpvm() {
+  if ! klist -s
+  then
+    kinit
+  fi
+
+  if [ $# -gt 0 ];
+  then
+    ssh -XY dphan@novagpvm$1.fnal.gov
+  else
+    ssh -XY dphan@novagpvm06.fnal.gov
+  fi
+}
+
+# Utility functions
+function cheat() {
+  curl cheat.sh/$1
+}
+
+# Using PyEnv
+if [ "$SYSTEMOS" = "Linux" ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+fi
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
